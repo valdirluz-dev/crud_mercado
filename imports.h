83d2e9ef279299bcb19142e6557d693d64d968b8
valdirluz-dev <valdirluz.dev@gmail.com>
2026-06-05 15:07:58 -0300

    estrutura inicial

---
+++ imports.h
@@ -0,0 +1,20 @@
+#ifndef IMPORTS_H
+#define IMPORTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct mercadoria
+{
+    char nome[20];
+    float preco;
+    char categoria[20];
+    int quantidade_disponivel;
+};
+
+void cadastrar_produto(void);
+void listar_produtos(void);
+void modificar_produto(void);
+void deletar_produto(void);
+
+#endif
---
+++ mercadorias.c
@@ -0,0 +1,17 @@
+#include "imports.h"
+
+void cadastrar_produto(void)
+{
+}
+
+void listar_produtos(void)
+{
+}
+
+void modificar_produto(void)
+{
+}
+
+void deletar_produto(void)
+{
+}
