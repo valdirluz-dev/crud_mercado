87f646b510410e4d92272d57121e4294fbeab4d8
PedroWilsonNegri <pedrowilson.dev@gmail.com>
2026-06-09 17:59:10 -0300

    Add Cadastro
    Adicionando método para cadastro dos produtos.

--- mercadorias.c
+++ mercadorias.c
@@ -2,6 +2,38 @@
 
 void cadastrar_produto(void)
 {
+  //cria o arquivo txt do cadastro de produtos - Pedro Wilson
+    FILE *arquivo = fopen("produtos.txt", "a");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return;
+    }
+
+    Produto p;
+    printf("\n--- Cadastrar Novo Produto ---\n");
+    
+    //limpa o buffer do teclado - Pedro Wilson
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF); 
+
+	//cadastro do produto - Pedro Wilson
+    printf("Nome do Produto: ");
+    fgets(p.nome, 30, stdin);
+    p.nome[strcspn(p.nome, "\n")] = 0;
+
+    printf("Preco: R$ ");
+    scanf("%f", &p.preco);
+
+    printf("Quantidade: ");
+    scanf("%d", &p.quantidade);
+
+    fprintf(arquivo, "%s:\n", p.nome);
+    fprintf(arquivo, "Preço: R$ %.2f\n", p.preco);
+    fprintf(arquivo, "Quantidade: %d\n", p.quantidade);
+    fprintf(arquivo, "------------------------\n");
+
+    fclose(arquivo);
+    printf("Produto salvo com sucesso!\n");
 }
 
 void listar_produtos(void)
