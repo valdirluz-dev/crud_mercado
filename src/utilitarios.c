125199d96a9ff3474c010de18821648c135075fb
valdirluz-dev <valdirluz.dev@gmail.com>
2026-06-17 15:24:04 -0300

    Move validar_produto para utilitarios.c para uso compartilhado

--- src/utilitarios.c
+++ src/utilitarios.c
@@ -4,3 +4,9 @@ void limpar_buffer(void) {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
+
+int validar_produto(Produto p) {
+    if (p.preco <= 0) return 0;
+    if (strlen(p.nome) < 3 || strlen(p.nome) > 30) return 0;
+    return 1;
+}
